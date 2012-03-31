42886cf9ba783095c62742d70cfd6ca03507bb5a
Marshall Ward <marshall.ward@gmail.com>
2012-03-31 15:32:29 +1100

    Whitespace changes

--- src/Map.cs
+++ src/Map.cs
@@ -36,10 +36,10 @@ namespace TiledSharp
         {
             var xml_doc = XDocument.Load(xml_path);
             var xml_map = xml_doc.Element("map");
-
+			
             version = (string)xml_map.Attribute("version");
             orientation = (string)xml_map.Attribute("orientation");
-
+			
             width = (int)xml_map.Attribute("width");
             height = (int)xml_map.Attribute("height");
             tilewidth = (int)xml_map.Attribute("tilewidth");
@@ -47,10 +47,10 @@ namespace TiledSharp
             
             foreach (var t in xml_map.Elements("tileset"))
                 tileset.Add(new Tileset(t));
-
+			
             foreach (var e in xml_map.Elements("layer"))
                 layer.Add(new Layer(e, width, height));
-
+			
             property = new PropertyDict(xml_map.Element("properties"));
         }
     }
@@ -60,7 +60,7 @@ namespace TiledSharp
         public PropertyDict(XElement xml_prop)
         {
             if (xml_prop == null) return;
-
+			
             foreach (var p in xml_prop.Elements("property"))
             {
                 var pname = (string)p.Attribute("name");
--- src/Tileset.cs
+++ src/Tileset.cs
@@ -13,11 +13,11 @@ namespace TiledSharp
         public int? tilewidth, tileheight;
         public int? spacing;
         public int? margin;
-
+		
         public Image image;
         public PropertyDict property;
         public Dictionary<int, PropertyDict> tile;
-
+		
         public Tileset(XElement xml_tileset)
         {
             name = (string)xml_tileset.Attribute("name");
@@ -25,17 +25,17 @@ namespace TiledSharp
             source = (string)xml_tileset.Attribute("source");
             tilewidth = (int?)xml_tileset.Attribute("tilewidth");
             tileheight = (int?)xml_tileset.Attribute("tileheight");
-
+			
             spacing = (int?)xml_tileset.Attribute("spacing");
             margin = (int?)xml_tileset.Attribute("margin");
-
+			
             if (source != null)
                 throw new Exception("Tiled: TSX tilesets unsupported.");
             else
                 image = new Image(xml_tileset.Element("image"));
-
+			
             property = new PropertyDict(xml_tileset.Element("properties"));
-
+			
             tile = new Dictionary<int, PropertyDict>();
             foreach (var xml_tile in xml_tileset.Elements("tile"))
             {
@@ -44,16 +44,16 @@ namespace TiledSharp
                 tile.Add(tid, new PropertyDict(xml_prop));
             }
         }
-
+		
         public class Image
         {
             public string source;
             public uint? trans;  // 24-bit RGB transparent color
-
+			
             public Image(XElement xml_image)
             {
                 source = (string)xml_image.Attribute("source");
-
+				
                 var xml_trans = (string)xml_image.Attribute("trans");
                 if (xml_trans != null)
                     trans = UInt32.Parse(xml_trans, NumberStyles.HexNumber);
