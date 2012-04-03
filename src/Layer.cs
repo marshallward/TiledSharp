7b87941dd304aee645a295a9f020bae62640a0ae
Marshall Ward <marshall.ward@gmail.com>
2012-04-03 21:42:45 +1000

    Layer Map Array replaced by Tile List

--- src/Layer.cs
+++ src/Layer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Linq;
 using System.IO;
 using System.IO.Compression;
@@ -11,8 +12,8 @@ namespace TiledSharp
         public double opacity = 1.0;
         public bool visible = true;
         
+        public List<Tile> tiles = new List<Tile>();
         public PropertyDict property;
-        public uint[,] data;
         
         public Layer(XElement xLayer, int width, int height)
         {
@@ -26,60 +27,73 @@ namespace TiledSharp
             if (xVisible != null)
                 visible = (bool)xVisible;
             
-            // Allocate the tile index map
-            data = new uint[width, height];
-            
             var xData = xLayer.Element("data");
-            switch ((string)xData.Attribute("encoding"))
+            var encoding = (string)xData.Attribute("encoding");
+            
+            if (encoding == "base64")
             {
-                case "base64":
-                    var base64data = Convert.FromBase64String((string)xData.Value);
-                    Stream stream = new MemoryStream(base64data, false);
-                    
-                    switch ((string)xData.Attribute("compression"))
-                    {
-                        case "gzip":
-                            stream = new GZipStream(stream, CompressionMode.Decompress, false);
-                            break;
-                        case "zlib":
-                            stream = new Ionic.Zlib.ZlibStream(stream, Ionic.Zlib.CompressionMode.Decompress, false);
-                            break;
-                        case null:
-                            // stream unmodified
-                            break;
-                        default:
-                            throw new Exception("Tiled: Unknown compression.");
-                    } // compression switch
-                    
-                    using (stream)
-                    using (var br = new BinaryReader(stream))
-                        for (int j = 0; j < height; j++)
-                            for (int i = 0; i < width; i++)
-                                data[i, j] = br.ReadUInt32();
-                    break;
-                case "csv":
-                    var csvData = (string)xData.Value;
-                    int k = 0;
-                    foreach (var s in csvData.Split(','))
-                    {
-                        data[k % width, k / width] = uint.Parse(s.Trim());
-                        k++;
-                    }
-                    break;
-                case null:
-                    k = 0;  // Declared in case "csv"
-                    foreach (var e in xData.Elements("tile"))
-                    {
-                        var gid = (int)e.Attribute("gid");
-                        data[k % width, k / width] = (uint)gid;
-                        k++;
-                    }
-                    break;
-                default:
-                    throw new Exception("Tiled: Unknown encoding.");
-            } // encoding switch
+                var base64data = Convert.FromBase64String((string)xData.Value);
+                Stream stream = new MemoryStream(base64data, false);
+                
+                var compression = (string)xData.Attribute("compression");
+                if (compression == "gzip")
+                    stream = new GZipStream(stream, CompressionMode.Decompress, false);
+                else if (compression == "zlib")
+                    stream = new Ionic.Zlib.ZlibStream(stream, Ionic.Zlib.CompressionMode.Decompress, false);
+                else if (compression != null)
+                    throw new Exception("Tiled: Unknown compression.");
+                
+                using (stream)
+                using (var br = new BinaryReader(stream))
+                    for (int j = 0; j < height; j++)
+                        for (int i = 0; i < width; i++)
+                            tiles.Add(new Tile(br.ReadUInt32(), i, j));
+            }
+            else if (encoding == "csv")
+            {
+                var csvData = (string)xData.Value;
+                int k = 0;
+                foreach (var s in csvData.Split(','))
+                {
+                    var gid = uint.Parse(s.Trim());
+                    var x = k % width;
+                    var y = k / width;
+                    tiles.Add(new Tile(gid, x, y));
+                    k++;
+                }
+            }
+            else if (encoding == null)
+            {
+                int k = 0;
+                foreach (var e in xData.Elements("tile"))
+                {
+                    var gid = (uint)e.Attribute("gid");
+                    var x = k % width;
+                    var y = k / width;
+                    tiles.Add(new Tile(gid, x, y));
+                    k++;
+                }
+            }
+            else throw new Exception("Tiled: Unknown encoding.");
             
             property = new PropertyDict(xLayer.Element("properties"));
+        }
+        
+        public class Tile
+        {
+            public uint gid;            // Global tile ID
+            public int x, y;            // Coordinate position
+            public bool hflip = false;  // Horizontal flip
+            public bool vflip = false;  // Vertical flip
+            public bool dflip = false;  // Diagonal flip 
+            
+            public Tile(uint id, int xi, int yi)
+            {
+                // Temporary
+                gid = id;
+                x = xi;
+                y = yi;
+            }
         }
     }
 }
