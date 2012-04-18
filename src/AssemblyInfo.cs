9b36640fb997aac0eede019387d09fa7dbda2662
Marshall Ward <marshall.ward@gmail.com>
2012-04-18 15:40:50 +1000

    Updated AssemblyInfo

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("TiledSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tiled TMX parsing library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("marshall")]
+[assembly: AssemblyProduct("TiledSharp")]
+[assembly: AssemblyCopyright("Copyright 2012 Marshall Ward")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-AU")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
