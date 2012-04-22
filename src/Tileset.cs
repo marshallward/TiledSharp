d9f5f055395a33b971fc270c51612a267e8e1824
Marshall Ward <marshall.ward@gmail.com>
2012-04-22 16:10:44 +1000

    Spewed Copyright notices

--- src/Tileset.cs
+++ src/Tileset.cs
@@ -1,3 +1,6 @@
+/* Distributed as part of TiledSharp, Copyright 2012 Marshall Ward
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
 using System;
 using System.Collections.Generic;
 using System.Xml.Linq;
