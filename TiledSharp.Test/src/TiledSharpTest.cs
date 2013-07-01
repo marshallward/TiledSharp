ef9e1a986a715d3cc42ce30e35679aa892994022
Marshall Ward <marshall.ward@gmail.com>
2013-07-01 22:53:08 +1000

    Added licensing info

--- TiledSharp.Test/src/TiledSharpTest.cs
+++ TiledSharp.Test/src/TiledSharpTest.cs
@@ -1,4 +1,6 @@
-using System;
+// Distributed as part of TiledSharp, Copyright 2012 Marshall Ward
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 using System.IO;
 using System.Reflection;
 using TiledSharp;
