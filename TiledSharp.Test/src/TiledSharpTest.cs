bbf09cf6c85acbbc7bd603cb2beadd79168eefd0
Marshall Ward <marshall.ward@gmail.com>
2013-07-01 22:45:36 +1000

    Unit testing cleanup; test asset inclusion

--- TiledSharp.Test/src/TiledSharpTest.cs
+++ TiledSharp.Test/src/TiledSharpTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using TiledSharp;
 using NUnit.Framework;
 
@@ -7,13 +9,27 @@ namespace TiledSharpTesting
     [TestFixture]
     public class TiledSharpTest
     {
+        const string examplePath = "assets";
+
         [Test]
         public void VersionTest()
         {
-            TmxMap map = new TmxMap("minimal.tmx");
+            var mapPath = GetAssetPath("minimal.tmx");
+
+            TmxMap map = new TmxMap(mapPath);
             string version = map.Version;
             Assert.AreEqual(version, "1.0");
         }
+
+        public string GetAssetPath(string mapFilename)
+        {
+            var exePath = Assembly.GetExecutingAssembly().Location;
+            var rootPath = Directory.GetParent(exePath).Parent.Parent.Parent;
+            var mapPath = Path.Combine(rootPath.ToString(), examplePath,
+                                       mapFilename);
+            return mapPath;
+        }
     }
+
 }
 
