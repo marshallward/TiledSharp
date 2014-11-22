49eb1b28e761385217c47997d0dc54daaa2d8db9
Marshall Ward <marshall.ward@gmail.com>
2014-11-22 16:20:33 +1100

    Assembly update

--- TiledSharp/src/AssemblyInfo.cs
+++ TiledSharp/src/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TiledSharp")]
-[assembly: AssemblyCopyright("Copyright 2012 Marshall Ward")]
+[assembly: AssemblyCopyright("Copyright 2012-2014 Marshall Ward")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.10.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
