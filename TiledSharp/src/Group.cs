066f57a11047ea72e624e7d1a20dd32209a7eb0f
Jakub Kurłowicz <jkurlowicz@gmail.com>
2017-08-04 19:47:20 +0200

    Added licence section

--- TiledSharp/src/Group.cs
+++ TiledSharp/src/Group.cs
@@ -1,4 +1,7 @@
-﻿using System;
+// Distributed as part of TiledSharp, Copyright 2012 Marshall Ward
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+using System;
 using System.Xml.Linq;
 
 namespace TiledSharp
